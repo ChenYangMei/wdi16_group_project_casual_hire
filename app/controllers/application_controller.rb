29c18c8f95d8a4e63d53e3044ca7686c2dc9e93d
gingernutter <yangmei.chen2011@gmail.com>
2016-09-18 21:52:59 +1000

    user and login session crud system done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :fetch_user
+
+  private
+  
+  def fetch_user
+    @current_user = User.find_by(:id => session[:user_id]) if session[:user_id]
+
+    session[:user_id] = nil unless @current_user.present?
+    # If there is a session user_id defined, find that user with the ID
+    # Assign it to an instance variable
+  end
 end
