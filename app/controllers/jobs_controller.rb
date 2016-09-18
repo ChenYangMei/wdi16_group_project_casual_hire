7097fd3e17298a01744caa30693ea5ef5a77acf2
Arpan B <arpanbhalla@gmail.com>
2016-09-18 20:36:11 +1000

    Jobs Basic CRUD made

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,32 +1,40 @@
 class JobsController < ApplicationController
-  def edit
-    @job = Job.find params[:id]
-  end
 
-  def index
-    @jobs = Job.all
-  end
+    def index
+      @jobs = Job.all
+    end
 
-  def new
-    @job = Job.create
-  end
+    def new
+      @job = Job.create
+    end
 
-  def show
-    @job = Job.find params[:id]
-  end
+    def create
+      job = Job.create( job_params )
+      redirect_to "/jobs/#{job.id}"
+    end
 
-  def update
+    def edit
+      @job = Job.find params[:id]
+    end
+
+    def update
+      job = Job.find params[:id]
+      job.update job_params
+      redirect_to job
+    end
+
+    def show
+      @job = Job.find params[:id]
+    end
+
+    def destroy
     job = Job.find params[:id]
-    job.update job_params
-  end
+    job.destroy
+    redirect_to jobs_path
+    end
 
-  def create
-    job = Job.create( job_params )
-    redirect_to job
-  end
-
-  private
-  def job_params
-    params.require(:job).permit(:task_title, :task_description, :task_location, :due_date, :start_time, :workers_required, :budget, :images )
-  end
+    private
+    def job_params
+      params.require(:job).permit(:task_title, :task_description, :task_location, :due_date, :start_time, :workers_required, :budget, :images )
+    end
 end
